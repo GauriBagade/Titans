252f6a1743dd1c5b13707907bb2506f914e0c2a7
GauriBagade <gbagade347@gmail.com>
2026-02-28 09:12:41 +0530

    android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.shantanu.farmfreshadvice',
   appName: 'Farm Fresh Advice',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#FFFFFF",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      splashFullScreen: true,
+      splashImmersive: false
+    }
+  }
 };
 
 export default config;
